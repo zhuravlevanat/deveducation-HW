bbe509796ee810dd79a724aefbcc15b24a2358b6
Zhuravlova Natalia <zhyravlevanat@gmail.com>
2019-11-14 13:03:45 +0200

    simple calc

--- lesson 2 calculator/script.js
+++ lesson 2 calculator/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const buttons = ['1', '2', '3', '4', '5', '6','7', '8', '9', '0', 
-                 '+', '-', '/', '=', '.', 'c'];
+const buttons = ['0','1', '2', '3', '4', '5', '6','7', '8', '9', '.',
+                 '+', '-', '*', '/', '=', '←', 'c'];
 
 const calcBtnsContainer = document.getElementById('calcBtnsContainer');
 const calcDisplay = document.getElementById('display');
 const bntItemTemplate = document.getElementById('bntItemTemplate').innerHTML;
+let inputs = ["","",""];
+let values = [];
 
 
 window.addEventListener('load', onPageLoaded);
@@ -17,25 +19,94 @@ function onPageLoaded() {
 
 function onCalcBntContainerClick(e) {
   const elem = e.target;
-  
-  if (elem.value === 'c') {
-    calcDisplay.value = '0';
-  } else if (elem.value === '=') {
-    calcDisplay.value = eval(calcDisplay.value);
-  } else if (calcDisplay.value === '0') {
-    calcDisplay.value = elem.value;
-  } else {
-    calcDisplay.value += elem.value;
-  }
+  const id = elem.dataset.id;
+
+  switch(true) {
+    case (id >= 0 && id <= 11): 
+      values.push(elem.value);
+      displayData();
+    break;
+    case (id >= 12 && id < 16): 
+      updateData(values.join(""));
+      updateData(elem.value);
+      values = [];
+      displayData();
+    break;
+    case (id == 16): 
+      updateData(values.join(""));
+      values = [];
+      calc();
+    break;
+    case (id == 17): 
+      deleteData();
+      displayData();
+    break;
+    case (id == 18): 
+      clearData();
+    break;
+  }  
 }
 
 function createCalcButtons() {
-  buttons.forEach(elem => {
+  buttons.forEach((elem, index) => {
     const buttonElement = bntItemTemplate.replace('{{value}}', elem)
-                                          .replace('{{sign}}', elem);                                  
+                                          .replace('{{sign}}', elem)
+                                          .replace('{{id}}', index+1);                                  
     calcBtnsContainer.innerHTML += buttonElement;
   })
 }
+
+const addNumbers = (firstNum, secondNum) => firstNum + secondNum;
+const multNumbers = (firstNum, secondNum) => firstNum * secondNum;
+const subsNumbers = (firstNum, secondNum) => firstNum - secondNum;
+const divNumbers = (firstNum, secondNum) => firstNum / secondNum;
+
+const calc = () => {
+  switch (inputs[1]) {
+    case "+": 
+      const sum = addNumbers(parseFloat(inputs[0]), parseFloat(inputs[2]));
+      clearData();	
+      values.push(sum);
+    break;
+    case "-": 
+      const diff = subsNumbers(parseFloat(inputs[0]), parseFloat(inputs[2]));
+      clearData();	
+      values.push(diff)
+    break;
+    case "*": 
+      const mult = multNumbers(parseFloat(inputs[0]), parseFloat(inputs[2]));
+      clearData();	
+      values.push(mult);
+    break;
+    case "/": 
+      const div = divNumbers(parseFloat(inputs[0]), parseFloat(inputs[2]));
+      clearData();
+      values.push(div);
+    break;
+  }  
+	displayData();
+}
+
+const updateData = (value) => {
+	inputs.push(value);
+	inputs.shift();
+}
+const clearData = () => {
+	inputs = ["","",""];
+	values = [];
+	calcDisplay.value ="";
+}
+const deleteData = () => {
+  if (values.length > 0) {
+    values.pop();
+  } 
+}
+const displayData = () => {
+	calcDisplay.value= inputs.join(" ") + " " + values.join("");
+}
+
+
+
 
 
         
